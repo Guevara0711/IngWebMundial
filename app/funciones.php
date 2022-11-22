17c3781778657bcf8024521167068fed59b3af51
Susanelizabetth <castillosusana55@gmail.com>
2022-11-21 21:12:13 -0500

    spasing

--- app/funciones.php
+++ app/funciones.php
@@ -1,13 +1,15 @@
 <?php
 session_start();
 
-class Usuarios{
-    public function login_usuario($a){
-        if($a == 1) {
+class Usuarios
+{
+    public function login_usuario($a)
+    {
+        if ($a == 1) {
             //$success = TRUE;
             //return $success;
             header("location: /Views/Equipos/Equipos.php");
-        }else {
+        } else {
             $success = FALSE;
             //return $success;
             $error = "*Usuario o contraseña incorrectos. Por favor verifique sus datos e intente nuevamente";
